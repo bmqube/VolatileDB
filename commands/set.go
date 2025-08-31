20b0546387619a60b5a3f4983303555689890fa7
bmqube <bmqube1@gmail.com>
2025-08-31 06:07:25 +0600

    added EX, PX options for set command

--- commands/set.go
+++ commands/set.go
@@ -2,6 +2,9 @@ package commands
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/bmqube/VolatileDB/models"
 	"github.com/bmqube/VolatileDB/resp"
@@ -18,13 +21,50 @@ func (cmd *SetCommand) Execute(args []models.Message, store *store.Storage, mb *
 	key := args[0]
 	val := args[1]
 
-	store.Set(string(key.BulkString), string(val.BulkString))
+	timeToLive := time.Duration(0)
+	for i := 2; i < len(args); i++ {
+		arg := args[i]
+
+		comm := strings.ToLower(string(arg.BulkString))
+
+		switch comm {
+		case "ex", "px":
+			if timeToLive > time.Duration(0) {
+				return mb.Error("ERR syntax error").Build()
+			}
+
+			if i+1 > len(args) {
+				return mb.Error("ERR syntax error").Build()
+			}
+
+			ttl, err := strconv.ParseInt(string(args[i+1].BulkString), 10, 64)
+
+			if err != nil {
+				return mb.Error("ERR value is not an integer or out of range").Build()
+			}
+
+			if ttl <= 0 {
+				return mb.Error("ERR value is not an integer or out of range").Build()
+			}
+
+			i++
+			if comm == "ex" {
+				timeToLive = time.Duration(ttl) * time.Second
+			} else {
+				timeToLive = time.Duration(ttl) * time.Millisecond
+			}
+		default:
+			return mb.Error("ERR syntax error").Build()
+		}
+	}
+
+	store.Set(string(key.BulkString), string(val.BulkString), timeToLive)
 
 	return mb.SimpleString("OK").Build()
 }
 
 func (cmd *SetCommand) ValidateArgs(args []models.Message) error {
-	if len(args) != 2 {
+	if len(args) < 2 {
 		return errors.New("ERR wrong number of arguments for 'set' command")
 	}
 
--- store/storage.go
+++ store/storage.go
@@ -4,48 +4,104 @@ import (
 	"errors"
 	"strconv"
 	"sync"
+	"time"
 )
 
 type Storage struct {
 	mu   sync.RWMutex
-	data map[string]string
+	data map[string]Entry
+}
+
+type Entry struct {
+	Value     string
+	ExpiresAt time.Time
 }
 
 func NewStorage() *Storage {
-	return &Storage{data: make(map[string]string)}
+	s := &Storage{
+		data: make(map[string]Entry),
+	}
+	s.startCleaner(1 * time.Second)
+	return s
+}
+
+func (s *Storage) startCleaner(interval time.Duration) {
+	go func() {
+		for {
+			time.Sleep(interval)
+			now := time.Now()
+
+			var deletableKeys []string
+
+			s.mu.RLock()
+			for k, e := range s.data {
+				if !e.ExpiresAt.IsZero() && now.After(e.ExpiresAt) {
+					deletableKeys = append(deletableKeys, k)
+				}
+			}
+			s.mu.RUnlock()
+
+			if len(deletableKeys) > 0 {
+				s.mu.Lock()
+
+				for _, k := range deletableKeys {
+					delete(s.data, k)
+				}
+
+				s.mu.Unlock()
+			}
+		}
+	}()
 }
 
 func (store *Storage) Get(key string) (string, bool) {
 	store.mu.RLock()
 	defer store.mu.RUnlock()
-	val, ok := store.data[key]
+	entry, ok := store.data[key]
 
-	return val, ok
+	if ok && !entry.ExpiresAt.IsZero() && time.Now().After(entry.ExpiresAt) {
+		store.mu.Lock()
+		delete(store.data, key)
+		store.mu.Unlock()
+		ok = false
+	}
+
+	if !ok {
+		return "", false
+	}
+
+	return entry.Value, true
 }
 
-func (store *Storage) Set(key string, val string) {
+func (store *Storage) Set(key string, val string, ttl time.Duration) {
 	store.mu.Lock()
 	defer store.mu.Unlock()
-	store.data[key] = val
+
+	var expiresAt time.Time
+	if ttl > 0 {
+		expiresAt = time.Now().Add(ttl)
+	}
+
+	store.data[key] = Entry{Value: val, ExpiresAt: expiresAt}
 }
 
 func (store *Storage) Incr(key string) (int64, error) {
 	store.mu.Lock()
 	defer store.mu.Unlock()
 
-	val, ok := store.data[key]
+	entry, ok := store.data[key]
 	if !ok {
-		store.data[key] = "0"
-		val = "0"
+		store.data[key] = Entry{Value: "0"}
+		entry = store.data[key]
 	}
 
-	intVal, err := strconv.ParseInt(val, 10, 64)
+	intVal, err := strconv.ParseInt(entry.Value, 10, 64)
 	if err != nil {
 		return 0, errors.New("ERR value is not an integer or out of range")
 	}
 
 	intVal++
-	store.data[key] = strconv.FormatInt(intVal, 10)
+	store.data[key] = Entry{Value: strconv.FormatInt(intVal, 10)}
 
 	return intVal, nil
 }
@@ -54,19 +110,19 @@ func (store *Storage) Decr(key string) (int64, error) {
 	store.mu.Lock()
 	defer store.mu.Unlock()
 
-	val, ok := store.data[key]
+	entry, ok := store.data[key]
 	if !ok {
-		store.data[key] = "0"
-		val = "0"
+		store.data[key] = Entry{Value: "0"}
+		entry = store.data[key]
 	}
 
-	intVal, err := strconv.ParseInt(val, 10, 64)
+	intVal, err := strconv.ParseInt(entry.Value, 10, 64)
 	if err != nil {
 		return 0, errors.New("ERR value is not an integer or out of range")
 	}
 
 	intVal--
-	store.data[key] = strconv.FormatInt(intVal, 10)
+	store.data[key] = Entry{Value: strconv.FormatInt(intVal, 10)}
 
 	return intVal, nil
 }
